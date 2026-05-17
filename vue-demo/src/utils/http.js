0545e2e221df207d0f757a5d017d2b8cec3f148c
gfs114 <15860827759@163.com>
2026-05-17 21:11:05 +0800

    滚回上一个版本

--- vue-demo/src/utils/http.js
+++ vue-demo/src/utils/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL: 'https://154.12.39.76:3000/api',
+    baseURL: 'http://154.12.39.76:3000/api',
     timeout: 8000
 })
 export default http
